7248e6010bc873297d9cddb81262278a5063098c
ayubsoft254 <henryoayub15@gmail.com>
2023-05-12 19:37:39 +0300

    issue fix 2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,13 @@ import datetime
 import json
 import os
 
+from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
 
 
 """FileStorage class module."""
